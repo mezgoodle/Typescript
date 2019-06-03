e204223e6a14f9a957a91fcb6876a55922071e6a
mezgoodle <mezgoodle@gmail.com>
2019-06-03 19:09:28 +0300

    Друга компіляція

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var q = 1;
+//змінні
+var str = 'string';
+var num = 10;
+var isTrue = true;
+isTrue = false;
+var unknown = '1';
+unknown = 1;
+var unknown1 = '1';
+unknown = 1;
--- index.ts
+++ index.ts
@@ -5,4 +5,7 @@ let num = 10;
 let isTrue: boolean = true;
 isTrue = false;
 
-let unknown = '1';
+let unknown: any = '1';
+unknown = 1;
+let unknown1: string | number = '1';
+unknown = 1;
