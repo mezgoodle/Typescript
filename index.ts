7fc68123e90355d22eaaaf8839f7fcd7ef256f9f
mezgoodle <mezgoodle@gmail.com>
2019-06-03 19:13:52 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,10 +11,10 @@ let unknown1: string | number = '1';
 unknown = 1;
 //масиви
 let numArray: number[] = [1, 2, 3];
-let numArray1: number[] = [1, 2, 3];
+let numArray1: Array<number> = [1, 2, 3];
 
 let strArray: string[] = ['1', '2', '3'];
-let strArray1: string[] = ['1', '2', '3'];
+let strArray1: Array<string> = ['1', '2', '3'];
 
 let boolArray: boolean[] = [true, false, true];
-let boolArray: boolean[] = [true, false, true];
+let boolArray1: Array<boolean> = [true, false, true];
