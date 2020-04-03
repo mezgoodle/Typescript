c6586faafd45f00298f765e492f11bb1e3b520e9
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-03 13:08:38 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
 const q = 1;
-//змінні
-let str: string = 'string';
+// variables
+let str: string = "string";
 let num = 10;
 let isTrue: boolean = true;
 isTrue = false;
 
-let unknown: any = '1';
+let unknown: any = "1";
 unknown = 1;
-let unknown1: string | number = '1';
+let unknown1: string | number = "1";
 unknown = 1;
-//масиви
+// arrays
 let numArray: number[] = [1, 2, 3];
 let numArray1: Array<number> = [1, 2, 3];
 
-let strArray: string[] = ['1', '2', '3'];
-let strArray1: Array<string> = ['1', '2', '3'];
+let strArray: string[] = ["1", "2", "3"];
+let strArray1: Array<string> = ["1", "2", "3"];
 
 let boolArray: boolean[] = [true, false, true];
 let boolArray1: Array<boolean> = [true, false, true];
 
 //tuples
-let array: [number, number, string] = [1, 2, '3'];
-let array1: [boolean, string, number] = [true, '2', 3];
+let array: [number, number, string] = [1, 2, "3"];
+let array1: [boolean, string, number] = [true, "2", 3];
